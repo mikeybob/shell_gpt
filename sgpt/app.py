c6323297d2736490697866a424ad16144aab1b8f
Farkhod Sadykov <farkhod.sadykov@gmail.com>
2023-12-19 08:21:01 +0100

    System role optimisations for GPT4+ models

--- sgpt/app.py
+++ sgpt/app.py
@@ -25,14 +25,14 @@ def main(
         help="Large language model to use.",
     ),
     temperature: float = typer.Option(
-        0.1,
+        0.0,
         min=0.0,
         max=2.0,
         help="Randomness of generated output.",
     ),
     top_probability: float = typer.Option(
         1.0,
-        min=0.1,
+        min=0.0,
         max=1.0,
         help="Limits highest probable tokens (words).",
     ),
